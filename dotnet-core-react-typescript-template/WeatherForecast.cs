7356c0c5db6645a27a2216077ab1a53a77e98f05
orbit0801 <orbit0801@gmail.com>
2021-04-05 13:24:47 +0900

    first commit

---
+++ dotnet-core-react-typescript-template/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace dotnet_core_react_typescript_template
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
